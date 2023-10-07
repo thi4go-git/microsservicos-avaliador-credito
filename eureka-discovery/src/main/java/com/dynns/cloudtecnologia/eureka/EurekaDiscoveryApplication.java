3803e3d0709e478265eca079d0d0153e0c71847d
thi4go-git <thi4go19@gmail.com>
2023-10-07 03:30:27 -0300

    configs eureka

--- eureka-discovery/src/main/java/com/dynns/cloudtecnologia/eureka/EurekaDiscoveryApplication.java
+++ eureka-discovery/src/main/java/com/dynns/cloudtecnologia/eureka/EurekaDiscoveryApplication.java
@@ -2,12 +2,14 @@ package com.dynns.cloudtecnologia.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaDiscoveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaDiscoveryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDiscoveryApplication.class, args);
+    }
 
 }
