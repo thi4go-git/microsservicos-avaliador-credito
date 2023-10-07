ba300a2071b86047b3a02d8d05b6d1d7114bd3b2
thi4go-git <thi4go19@gmail.com>
2023-10-07 03:24:52 -0300

    Criando Eureka Server e configurando POM

---
+++ eureka-discovery/src/main/java/com/dynns/cloudtecnologia/eureka/EurekaDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.dynns.cloudtecnologia.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryApplication.class, args);
+	}
+
+}
---
+++ eureka-discovery/src/test/java/com/dynns/cloudtecnologia/eureka/EurekaDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynns.cloudtecnologia.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
