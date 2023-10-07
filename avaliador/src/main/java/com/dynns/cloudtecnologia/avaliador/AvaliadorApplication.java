d83547fbc00d28334b3dbf553bb6081edef19260
thi4go-git <thi4go19@gmail.com>
2023-10-07 13:47:19 -0300

    Criado o MS Avaliador

---
+++ avaliador/src/main/java/com/dynns/cloudtecnologia/avaliador/AvaliadorApplication.java
@@ -0,0 +1,13 @@
+package com.dynns.cloudtecnologia.avaliador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliadorApplication.class, args);
+	}
+
+}
